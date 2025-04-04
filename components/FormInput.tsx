45fa52153e4ed143fb87ff508b262a138bd8d5b1
Mateus <mateus.olvr2002@gmail.com>
2025-04-04 14:28:30 -0300

    style: alteração no estilo do componente 'FormInput.tsx'

--- components/FormInput.tsx
+++ components/FormInput.tsx
@@ -59,7 +59,7 @@ const styles = StyleSheet.create({
   inputWrapper: {
     flexDirection: "row",
     alignItems: "center",
-    padding: 16,
+    padding: 8,
     borderRadius: 8,
     borderWidth: 1,
     borderColor: "#D6D6D6",
