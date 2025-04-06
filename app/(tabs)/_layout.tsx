be6045db012b2aaa7f21bd7cf60c23e358ada4e3
Mateus <mateus.olvr2002@gmail.com>
2025-04-06 18:10:51 -0300

    style: alteração no estilo da barra de navegação

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -7,14 +7,14 @@ export default function TabsLayout() {
     <Tabs
       screenOptions={{
         headerShown: false,
-        tabBarStyle: {
-          height: 80,
-          borderTopWidth: 0,
-          backgroundColor: "#fff",
-          elevation: 0,
+        tabBarStyle: {            
+            height: 65,
+            borderTopWidth: 0,
+            backgroundColor: "#fff",
+            elevation: 0,
         },
         tabBarLabelStyle: {
-          fontSize: 16, 
+          fontSize: 14, 
         },
         tabBarActiveTintColor: "#FE8C00",
       }}
@@ -22,19 +22,19 @@ export default function TabsLayout() {
       <Tabs.Screen name="home" options={{ 
             title: "Início", 
             tabBarIcon: ({ color }) => (
-              <Icon name="home" size={24} color={color} />
+              <Icon name="home" size={20} color={color} />
             ),
         }} />
       <Tabs.Screen name="cart" options={{ 
             title: "Carrinho",
             tabBarIcon: ({ color }) => (
-                <Icon name="cart" size={24} color={color} />
+                <Icon name="cart" size={20} color={color} />
             ),
         }} />
       <Tabs.Screen name="profile" options={{ 
             title: "Perfil",
             tabBarIcon: ({ color }) => (
-                <Icon name="person" size={24} color={color} />
+                <Icon name="person" size={20} color={color} />
             ), 
         }} />
     </Tabs>
--- components/__tests__/BottomBar.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react-native";
-import { BottomBar } from "../BottomBar";
-
-describe("BottomBar Component", () => {
-    test("the component rendered", () => {
-        render(<BottomBar />);
-    })
-});
